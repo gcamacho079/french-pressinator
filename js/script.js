6cd1fe3a257c4fc52dbf395398250ebc942c8663
Guadalupe Camacho <gcamacho079@gmail.com>
2017-03-01 15:41:06 -0800

    Timers fixed; beginning reorganization of functions and jQuery events

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   var timeTotal = 60; // In seconds
   var minutes;
   var seconds;
@@ -11,7 +11,7 @@ $(document).ready(function() {
    Timer for Cooling Boiled Water
    - Begins when user pushes button
    - After timer stops, prompts user to begin pouring water into press
-  *********************************************************/
+  *********************************************************
 
   function waterCooler() {
 
@@ -24,7 +24,17 @@ $(document).ready(function() {
       clearInterval(waterTimer);
       console.log("Your water is now at the optimal temperature.");
       timeTotal = 240;
-      var coffeeTimer =  setInterval(brewClock, 1000);
+      var coffeeTimer =  setInterval(function() {
+        if (timeTotal >= 0) {
+          stringifyTime();
+          console.log(timeRemaining);
+          timeTotal--;
+        }
+        else {
+          console.log("Your coffee is ready! Now push the plunger down slowly...");
+          clearInterval(coffeeTimer);
+        }
+      }, 1000);
     }
   }
 
@@ -34,22 +44,14 @@ $(document).ready(function() {
    - Prompts stir 30 seconds into brew
   *********************************************************/
 
-  function brewClock() {
-    if (timeTotal >= 0) {
-      stringifyTime();
-      console.log(timeRemaining);
-      timeTotal--;
-    }
-    else {
-      console.log("Your coffee is ready! Now push the plunger down slowly...");
-      clearInterval(coffeeTimer);
-    }
+  //function brewClock() {
 
-  }
+
+  //}
 
   /*********************************************************
    Time Formatters
-  *********************************************************/
+  *********************************************************
 
   function stringifyTime() {
     if (timeTotal < 60) {
@@ -78,4 +80,69 @@ $(document).ready(function() {
     }
   }
 
-})
+})*/
+
+var timeTotal = 60; // Unit (s)
+var minutes;
+var seconds;
+var end = 0;
+var timeRemaining = "";
+
+var frenchPress = {
+  onReady: function() {
+    $("#startTimer").click(frenchPress.waterCooler);
+  },
+
+  /*********************************************************
+   Water and Coffee Timers
+  *********************************************************/
+
+  waterCooler: function() {
+    var waterTimer = setInterval(function() {
+      if (timeTotal >= 0) {
+        frenchPress.stringifyTime();
+        console.log(timeRemaining);
+        timeTotal--;
+      }
+      else {
+        console.log("Your water is now at the optimal temperature.");
+        clearInterval(waterTimer);
+      }
+    }, 1000);
+  },
+
+  /*********************************************************
+   Time Formatters
+  *********************************************************/
+
+  stringifyTime: function() {
+    if (timeTotal < 60) {
+      minutes = "00";
+      seconds = timeTotal;
+      frenchPress.secondFormatter();
+    }
+    else {
+      frenchPress.minuteFormatter();
+    }
+    timeRemaining = minutes + ":" + seconds;
+  },
+
+
+  minuteFormatter: function() {
+    minutes = Math.floor(timeTotal/60);
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    seconds = timeTotal - (minutes * 60);
+    frenchPress.secondFormatter();
+  },
+
+
+  secondFormatter: function() {
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+  }
+};
+
+$(document).ready(frenchPress.onReady);
